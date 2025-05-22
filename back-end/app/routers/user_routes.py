1c4517673368b61b5001ff645988abbcac7c7242
HuyHoang1977 <dotrinhhuyhoang1977@gmail.com>
2025-05-22 10:33:12 +0700

    firsts FE commit

--- back-end/app/routers/user_routes.py
+++ back-end/app/routers/user_routes.py
@@ -1,9 +1,18 @@
 from flask import Blueprint, request, jsonify
+from app.models.models import User
 from app.services.user_service import login_user, register_user
 from app.validations.user_validation import validate_login_data, validate_register_data
 
 bp = Blueprint('auth_routes', __name__)
 
+@bp.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([
+        {"id": u.id, "name": u.name, "email": u.email}
+        for u in users
+    ])
+    
 @bp.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
